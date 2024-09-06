4681067eb711e1fbbc28f26ef661dd4216ff5feb
ck_kho <33413239+chaikiat@users.noreply.github.com>
2024-09-06 13:56:24 +1000

    Update score_card.sql

--- grafana/score_card.sql
+++ grafana/score_card.sql
@@ -32,6 +32,6 @@ SELECT
 ,try
 ,usd
 ,zar
-FROM free_currency_aud_data_parquet_tbl_prod_2024_09_04_09_31_52_414257 
+FROM free_currency_aud_data_parquet_tbl_prod_2024_09_06_02_39_47_397379 
 WHERE $__timeFilter(CAST(day_date AS DATE)) 
 ORDER BY 1 
