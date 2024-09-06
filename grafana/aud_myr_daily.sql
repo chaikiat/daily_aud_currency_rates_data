2bf8467c7dd93d2499bed8a6aed3501e01aff3f7
ck_kho <33413239+chaikiat@users.noreply.github.com>
2024-09-06 13:56:10 +1000

    Update aud_myr_daily.sql

--- grafana/aud_myr_daily.sql
+++ grafana/aud_myr_daily.sql
@@ -1,6 +1,6 @@
 SELECT
   CAST(day_date AS DATE) AS "date"
   ,myr
-FROM free_currency_aud_data_parquet_tbl_prod_2024_09_04_09_31_52_414257 
+FROM free_currency_aud_data_parquet_tbl_prod_2024_09_06_02_39_47_397379 
 WHERE $__timeFilter(CAST(day_date AS DATE)) 
 ORDER BY 1 
